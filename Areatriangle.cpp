5635461db84db8dc1bea1c67ff7deadc0df25673
SanyaUa2003 <71663207+SanyaUa2003@users.noreply.github.com>
2020-09-21 21:38:39 +0300

    Add files via upload

---
+++ Areatriangle.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <cmath>
+
+int main()
+{
+    double sedi_a = 0.0;
+    std::cin >> sedi_a;
+
+    double sedi_b = 0.0;
+    std::cin >> sedi_b;
+
+    double sedi_c = 0.0;
+    std::cin >> sedi_c;
+
+    double semiperimeter;
+    semiperimeter = (sedi_a + sedi_b + sedi_c) / 2;
+
+    double perimeter;
+    perimeter = semiperimeter * (semiperimeter + sedi_a) * (semiperimeter + sedi_b) * (semiperimeter + sedi_c);
+
+    std::cout << sqrt(perimeter) << std::endl;
+
+    return 0;
+}
